19fac8506faf2ec3d2d39c692d2e7b8d3edd8549
KareemAttiyah <karimattia98@yahoo.com>
2020-12-04 01:26:27 +0200

    Transformation Component tranform and transform children problrm

--- source/common/components/transformationComponent.hpp
+++ source/common/components/transformationComponent.hpp
@@ -35,20 +35,37 @@ public:
                    const glm::vec3 &scale = {1, 1, 1})
     {
         glm::mat4 transformationMatrix = calculateTransformationMatrix(translation, rotation, scale);
-        tranformChildren(this, transfomrationMatrix);
+
+        std::cout << transformationMatrix[0][0] << " " << transformationMatrix[0][1] << " " << transformationMatrix[0][2] << " " << transformationMatrix[0][3] << endl;
+        std::cout << transformationMatrix[1][0] << " " << transformationMatrix[1][1] << " " << transformationMatrix[1][2] << " " << transformationMatrix[1][3] << endl;
+        std::cout << transformationMatrix[2][0] << " " << transformationMatrix[2][1] << " " << transformationMatrix[2][2] << " " << transformationMatrix[2][3] << endl;
+        std::cout << transformationMatrix[3][0] << " " << transformationMatrix[3][1] << " " << transformationMatrix[3][2] << " " << transformationMatrix[3][3] << endl;
+        std::cout << endl;
+        transformChildren(this, transfomrationMatrix);
     }
 
-    void tranformChildren(TransformationComponent *parent, glm::mat4 &transformationMatrix)
+    void transformChildren(TransformationComponent *parent, glm::mat4 &transformationMatrix2)
     {
-        parent->transfomrationMatrix *= transformationMatrix; // M
 
+        std::cout << transformationMatrix2[0][0] << " " << transformationMatrix2[0][1] << " " << transformationMatrix2[0][2] << " " << transformationMatrix2[0][3] << endl;
+        std::cout << transformationMatrix2[1][0] << " " << transformationMatrix2[1][1] << " " << transformationMatrix2[1][2] << " " << transformationMatrix2[1][3] << endl;
+        std::cout << transformationMatrix2[2][0] << " " << transformationMatrix2[2][1] << " " << transformationMatrix2[2][2] << " " << transformationMatrix2[2][3] << endl;
+        std::cout << transformationMatrix2[3][0] << " " << transformationMatrix2[3][1] << " " << transformationMatrix2[3][2] << " " << transformationMatrix2[3][3] << endl;
+        std::cout << endl;
+        parent->transfomrationMatrix *= transformationMatrix2; // M
+        glm::mat4 temp = parent->getTransformationMatrix();
+        std::cout << temp[0][0] << " " << temp[0][1] << " " << temp[0][2] << " " << temp[0][3] << endl;
+        std::cout << temp[1][0] << " " << temp[1][1] << " " << temp[1][2] << " " << temp[1][3] << endl;
+        std::cout << temp[2][0] << " " << temp[2][1] << " " << temp[2][2] << " " << temp[2][3] << endl;
+        std::cout << temp[3][0] << " " << temp[3][1] << " " << temp[3][2] << " " << temp[3][3] << endl;
+        std::cout << endl;
         // Base condition
         if (!(parent->hasChildren()))
             return;
 
         // General case
         for (int i = 0; i < parent->children.size(); i++)
-            tranformChildren(parent->children[i], transfomrationMatrix);
+            transformChildren(parent->children[i], transfomrationMatrix);
     }
 
     const glm::mat4 getTransformationMatrix()
